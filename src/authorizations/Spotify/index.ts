b5d49da5d06c9382eba3a578325f6d0d5fae55af
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-12-06 11:43:38 +0000

    added connection to backend with playlist import

--- src/authorizations/Spotify/index.ts
+++ src/authorizations/Spotify/index.ts
@@ -119,7 +119,55 @@ export const getSpotifyPlaylists = async (userData: any) => {
   return spotifyPlaylists;
 };
 
-export const importSpotifyPlaylist = async (playlistId: string) => {
+export const createImportUrl = (playlistId: string, offset: number = 0) => {
+  const importUrl =
+    "https://api.spotify.com/v1/playlists/" +
+    playlistId +
+    "/tracks?offset=" +
+    offset +
+    "&limit=100&fields=total,offset,next,limit,href,items(track(name,artists(name),album(name,images),preview_url))";
+
+  return importUrl;
+};
+
+export const importPlaylistToDb = async (
+  tracksArray: any[],
+  userId: string,
+  playlistName: string
+) => {
+  const finalPlaylist = {
+    user: userId,
+    playlist: {
+      tracks: [...tracksArray],
+      name: playlistName.toString(),
+    },
+  };
+
+  const config = {
+    method: "POST",
+    body: JSON.stringify(finalPlaylist),
+    headers: new Headers({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("accessToken"),
+    }),
+  };
+
+  const response = await fetch(
+    process.env.REACT_APP_BE_URL + "/playlists",
+    config
+  );
+
+  const playlistId = response.json();
+  console.log(playlistId);
+};
+
+export const loadNextTracks = async (
+  nextUrl: string,
+  masterTracksArray: any[],
+  totalTracks: number,
+  userId: string,
+  playlistName: string
+) => {
   const config = {
     headers: new Headers({
       "Content-Type": "application/json",
@@ -127,13 +175,84 @@ export const importSpotifyPlaylist = async (playlistId: string) => {
     }),
   };
 
-  const response = await fetch(
-    "https://api.spotify.com/v1/playlists/" +
-      playlistId +
-      "/tracks?fields=total,offset,next,limit,href,items(track(name,artists(name),album(name,images),preview_url))",
-    config
-  );
+  const response = await fetch(nextUrl, config);
+  const data = await response.json();
+  masterTracksArray.push(...data.items);
+  console.log(masterTracksArray);
+  if (masterTracksArray.length === totalTracks) {
+    importPlaylistToDb(masterTracksArray, userId, playlistName);
+  }
+  return data;
+};
+
+export const createTrackLoadPromise = (
+  nextUrl: string,
+  masterTracksArray: any[],
+  totalTracks: number,
+  userId: string,
+  playlistName: string
+) => {
+  return new Promise((resolve, reject) => {
+    loadNextTracks(
+      nextUrl,
+      masterTracksArray,
+      totalTracks,
+      userId,
+      playlistName
+    );
+  });
+};
+
+export const importSpotifyPlaylist = async (
+  playlistId: string,
+  totalTracks: number,
+  userId: string,
+  playlistName: string
+) => {
+  let playlistTracksMaster: any[] = [];
+
+  const config = {
+    headers: new Headers({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("spotifyAccessToken"),
+    }),
+  };
+
+  const response = await fetch(createImportUrl(playlistId), config);
+
   const playlistTracks = await response.json();
 
-  console.log(playlistTracks);
+  playlistTracksMaster = [...playlistTracks.items];
+
+  const { total, limit } = playlistTracks;
+
+  const numberOfIterationsLeft = Math.ceil(total / limit);
+
+  console.log(
+    "Number of iterations left to be done: " + numberOfIterationsLeft
+  );
+
+  console.log(playlistTracks.next);
+
+  const promiseArray: any[] = [];
+
+  for (let i = 0; i < numberOfIterationsLeft; i++) {
+    const offsetNum = (i + 1) * 100;
+
+    promiseArray.push(
+      createTrackLoadPromise(
+        createImportUrl(playlistId, offsetNum),
+        playlistTracksMaster,
+        totalTracks,
+        userId,
+        playlistName
+      )
+    );
+  }
+
+  console.log("finished making promises array");
+
+  for (const elem of promiseArray) {
+    let result = await elem;
+  }
 };
--- src/components/pages/Import/ImportPlaylist.tsx
+++ src/components/pages/Import/ImportPlaylist.tsx
@@ -7,6 +7,7 @@ import {
   onPageLoad,
   requestSpotifyAuthorization,
 } from "../../../authorizations/Spotify";
+import { useAppSelector } from "../../../redux/store/hooks";
 
 interface spotifyData {
   user: any;
@@ -16,22 +17,34 @@ interface spotifyData {
 const ImportPlaylist = () => {
   const [spotifyData, setSpotifyData] = useState<spotifyData | null>(null);
 
+  const currentUser = useAppSelector((state) => {
+    return state.loadedProfile.currentUser;
+  });
+
   useEffect(() => {
     getSpotifyData();
     console.log(spotifyData);
   }, []);
 
   const getSpotifyData = async () => {
-    let tempCode = onPageLoad()!;
-    console.log(tempCode);
-    if (tempCode !== "") {
-      let response = await fetchSpotifyAccessToken(tempCode, "/import");
+    if (localStorage.getItem("spotifyCode")) {
+      let response = await fetchSpotifyAccessToken(
+        localStorage.getItem("spotifyCode")!,
+        "/import"
+      );
       console.log(response);
       setSpotifyData(response);
+    } else {
+      let tempCode = onPageLoad()!;
+      console.log(tempCode);
+      if (tempCode !== "") {
+        localStorage.setItem("spotifyCode", tempCode);
+        let response = await fetchSpotifyAccessToken(tempCode, "/import");
+        console.log(response);
+        setSpotifyData(response);
+      }
     }
   };
-
-
 
   return (
     <div
@@ -44,57 +57,64 @@ const ImportPlaylist = () => {
       }}
     >
       <h1 className="text-white mb-5">Import Playlist</h1>
-      {spotifyData !== null ? (
+      {localStorage.getItem("spotifyAccessToken") && spotifyData ? (
         <>
-        {spotifyData.playlists.next && (
-                <div className="text-white">
-                  <p>{spotifyData.playlists.total + " playlists found"}</p>
-                </div>
-              )}
-        <Container
-          style={{
-            boxShadow: "inset 5px 5px 15px 5px rgba(0,0,0,0.23)",
-            borderRadius: "30px",
-            height: "60vh",
-            background: "black",
-            opacity: 0.75,
-          }}
-          className="overflow-auto text-white "
-        >
-          {spotifyData.playlists.items.length === 0 ? (
-            <p className="pt-5 text-white">No playlists found on profile...</p>
-          ) : (
-            <>              
-              {spotifyData.playlists.items.map((playlist: any) => (
-                <div key={playlist.id}>
-                  <Row className="my-4">
-                    <Col>
-                      <Image
-                        style={{ borderRadius: "500px", height: "5rem" }}
-                        src={playlist.images[0].url}
-                      />
-                    </Col>
-                    <Col className="my-4">{playlist.name}</Col>
-                    <Col className="my-4">
-                      {playlist.tracks.total + " tracks"}
-                    </Col>
-                    <Col>
-                      <Button
-                        className="mt-3"
-                        onClick={() => {
-                          importSpotifyPlaylist(playlist.id);
-                        }}
-                      >
-                        Import
-                      </Button>
-                    </Col>
-                  </Row>{" "}
-                  <hr></hr>
-                </div>
-              ))}
-            </>
+          {spotifyData.playlists.next && (
+            <div className="text-white">
+              <p>{spotifyData.playlists.total + " playlists found"}</p>
+            </div>
           )}
-        </Container>
+          <Container
+            style={{
+              boxShadow: "inset 5px 5px 15px 5px rgba(0,0,0,0.23)",
+              borderRadius: "30px",
+              height: "60vh",
+              background: "black",
+              opacity: 0.75,
+            }}
+            className="overflow-auto text-white "
+          >
+            {spotifyData.playlists.items.length === 0 ? (
+              <p className="pt-5 text-white">
+                No playlists found on profile...
+              </p>
+            ) : (
+              <>
+                {spotifyData.playlists.items.map((playlist: any) => (
+                  <div key={playlist.id}>
+                    <Row className="my-4">
+                      <Col>
+                        <Image
+                          style={{ borderRadius: "500px", height: "5rem" }}
+                          src={playlist.images[0].url}
+                        />
+                      </Col>
+                      <Col className="my-4">{playlist.name}</Col>
+                      <Col className="my-4">
+                        {playlist.tracks.total + " tracks"}
+                      </Col>
+                      <Col>
+                        <Button
+                          className="mt-3"
+                          onClick={() => {
+                            importSpotifyPlaylist(
+                              playlist.id,
+                              playlist.tracks.total,
+                              currentUser._id,
+                              playlist.name
+                            );
+                          }}
+                        >
+                          Import
+                        </Button>
+                      </Col>
+                    </Row>{" "}
+                    <hr></hr>
+                  </div>
+                ))}
+              </>
+            )}
+          </Container>
         </>
       ) : (
         <Button onClick={() => requestSpotifyAuthorization("/import")}>
